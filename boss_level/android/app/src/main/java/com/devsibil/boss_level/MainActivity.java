03e19fd9de00867088626bb523b5febeabcaec24
SibilSoren <63915183+SibilSoren@users.noreply.github.com>
2021-02-02 16:36:42 +0530

    Intial Commit

---
+++ boss_level/android/app/src/main/java/com/devsibil/boss_level/MainActivity.java
@@ -0,0 +1,6 @@
+package com.devsibil.boss_level;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ boss_level/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: FlatButton(
+            onPressed: (){
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
